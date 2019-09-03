34cf217f321809b8b8ab70d8bdf38fc40f3a37d3
LucasVanden <vandenboschlucas@hotmail.com>
2019-09-02 22:59:45 -0300

    update BD, libreria jquery

--- consultasfrm.sql
+++ consultasfrm.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.7.4
+-- version 4.7.0
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 23-08-2019 a las 03:12:02
--- Versión del servidor: 10.1.28-MariaDB
--- Versión de PHP: 7.1.11
+-- Tiempo de generación: 03-09-2019 a las 03:54:55
+-- Versión del servidor: 10.1.25-MariaDB
+-- Versión de PHP: 7.1.7
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
@@ -29,9 +29,10 @@ SET time_zone = "+00:00";
 --
 
 CREATE TABLE `alumno` (
+  `id_alumno` int(20) NOT NULL,
+  `legajo` int(50) NOT NULL,
   `apellido` varchar(50) NOT NULL,
   `nombre` varchar(50) NOT NULL,
-  `legajo` int(50) NOT NULL,
   `email` varchar(50) NOT NULL,
   `fechaNacimientoAlumno` date NOT NULL,
   `telefonoAlumno` varchar(50) NOT NULL
@@ -44,8 +45,11 @@ CREATE TABLE `alumno` (
 --
 
 CREATE TABLE `anotadosestado` (
+  `id_anotadoestado` int(20) NOT NULL,
   `fechaAnotadosEstado` date NOT NULL,
-  `horaAnotadosEstado` time NOT NULL
+  `horaAnotadosEstado` time NOT NULL,
+  `fk_estadoanotados` int(11) NOT NULL,
+  `fk_detalleanotados` int(20) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
@@ -55,10 +59,18 @@ CREATE TABLE `anotadosestado` (
 --
 
 CREATE TABLE `asueto` (
+  `id_asueto` int(20) NOT NULL,
+  `fechaAsueto` date NOT NULL,
   `horaDesdeAsueto` time NOT NULL,
-  `horaHastaAsueto` time NOT NULL,
-  `fechaAsueto` date NOT NULL
+  `horaHastaAsueto` time NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Volcado de datos para la tabla `asueto`
+--
+
+INSERT INTO `asueto` (`id_asueto`, `fechaAsueto`, `horaDesdeAsueto`, `horaHastaAsueto`) VALUES
+(4, '2019-07-19', '00:26:16', '00:26:16');
 
 -- --------------------------------------------------------
 
@@ -79,8 +91,10 @@ CREATE TABLE `aula` (
 --
 
 CREATE TABLE `avisoprofesor` (
+  `id_avisoprofesor` int(20) NOT NULL,
+  `fechaAvisoProfesor` date NOT NULL,
   `detalleDescripcion` varchar(500) NOT NULL,
-  `fechaAvisoProfesor` date NOT NULL
+  `fk_horadeconsulta` int(20) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
@@ -90,8 +104,21 @@ CREATE TABLE `avisoprofesor` (
 --
 
 CREATE TABLE `dedicacion` (
+  `id_dedicacion` int(16) NOT NULL,
   `tipo` varchar(20) NOT NULL,
   `cantidadHora` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `dedicacion_materia_profesor`
+--
+
+CREATE TABLE `dedicacion_materia_profesor` (
+  `id_dedicacion_materia_profesor` int(20) NOT NULL,
+  `fk_dedicacion` int(20) NOT NULL,
+  `fk_materia` int(20) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
@@ -101,6 +128,7 @@ CREATE TABLE `dedicacion` (
 --
 
 CREATE TABLE `departamento` (
+  `id_departamento` int(20) NOT NULL,
   `nombre` varchar(50) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
@@ -111,9 +139,23 @@ CREATE TABLE `departamento` (
 --
 
 CREATE TABLE `detalleanotados` (
+  `id_detalleanotados` int(20) NOT NULL,
   `fechaDesdeAnotados` date NOT NULL,
   `horaDetalleAnotados` time(6) NOT NULL,
-  `tema` varchar(500) NOT NULL
+  `tema` varchar(500) NOT NULL,
+  `fk_alumno` int(20) NOT NULL,
+  `fk_horadeconsulta` int(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `dia`
+--
+
+CREATE TABLE `dia` (
+  `id_dia` int(20) NOT NULL,
+  `dia` varchar(50) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
@@ -123,6 +165,7 @@ CREATE TABLE `detalleanotados` (
 --
 
 CREATE TABLE `estadoanotados` (
+  `id_estadoanotados` int(20) NOT NULL,
   `nombreEstado` varchar(15) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
@@ -133,11 +176,15 @@ CREATE TABLE `estadoanotados` (
 --
 
 CREATE TABLE `horadeconsulta` (
+  `id_horadeconsulta` int(20) NOT NULL,
   `fechaDesdeAnotados` date NOT NULL,
   `fechaHastaAnotados` date NOT NULL,
   `cantidadAnotados` int(50) NOT NULL,
   `estadoPresentismo` varchar(50) NOT NULL,
-  `estadoVigencia` varchar(50) NOT NULL
+  `estadoVigencia` varchar(50) NOT NULL,
+  `fk_materia` int(20) NOT NULL,
+  `fk_presentismo` int(20) NOT NULL,
+  `fk_horariodeconsulta` int(20) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
@@ -147,11 +194,15 @@ CREATE TABLE `horadeconsulta` (
 --
 
 CREATE TABLE `horariocursado` (
+  `id_horariocursado` int(20) NOT NULL,
   `HoraDesde` date NOT NULL,
-  `HoraHasta` date NOT NULL,
-  `Dia` varchar(50) NOT NULL,
   `comision` varchar(50) NOT NULL,
-  `semestreAnula` varchar(50) NOT NULL
+  `semestreAnula` varchar(50) NOT NULL,
+  `fk_materia` int(20) NOT NULL,
+  `HoraHasta` date NOT NULL,
+  `fk_dia` int(20) NOT NULL,
+  `fk_profesor` int(20) NOT NULL,
+  `fk_turno` int(20) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
@@ -161,22 +212,14 @@ CREATE TABLE `horariocursado` (
 --
 
 CREATE TABLE `horariodeconsulta` (
-  `dia` varchar(10) NOT NULL,
+  `id_horariodeconsulta` int(20) NOT NULL,
   `hora` varchar(10) NOT NULL,
   `activoDesde` date NOT NULL,
   `activoHasta` date NOT NULL,
-  `semestre` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
--- --------------------------------------------------------
-
---
--- Estructura de tabla para la tabla `horariodeconsultamesa`
---
-
-CREATE TABLE `horariodeconsultamesa` (
-  `dia` varchar(20) NOT NULL,
-  `hora` varchar(10) NOT NULL
+  `semestre` int(11) NOT NULL,
+  `fk_dia` int(20) NOT NULL,
+  `fk_profesor` int(20) NOT NULL,
+  `fk_materia` int(20) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
@@ -186,18 +229,21 @@ CREATE TABLE `horariodeconsultamesa` (
 --
 
 CREATE TABLE `materia` (
-  `nombreMateria` varchar(50) NOT NULL
+  `id_materia` int(20) NOT NULL,
+  `nombreMateria` varchar(50) NOT NULL,
+  `fk_departamento` int(20) NOT NULL,
+  `fk_dia` int(20) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
 
 --
--- Estructura de tabla para la tabla `mesas`
+-- Estructura de tabla para la tabla `materias_alumno`
 --
 
-CREATE TABLE `mesas` (
-  `Dia` varchar(50) NOT NULL,
-  `fecha` date NOT NULL
+CREATE TABLE `materias_alumno` (
+  `fk_alumno` int(20) NOT NULL,
+  `fk_materia` int(20) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
@@ -207,7 +253,8 @@ CREATE TABLE `mesas` (
 --
 
 CREATE TABLE `perfil` (
-  `nombrePerfil` varchar(20) NOT NULL
+  `nombrePerfil` varchar(20) NOT NULL,
+  `id_perfil` int(20) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
@@ -217,9 +264,11 @@ CREATE TABLE `perfil` (
 --
 
 CREATE TABLE `presentismo` (
+  `id_presentismo` int(20) NOT NULL,
   `fecha` date NOT NULL,
   `horaDesde` time NOT NULL,
-  `horaHasta` time NOT NULL
+  `horaHasta` time NOT NULL,
+  `fk_profesor` int(20) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
@@ -230,7 +279,8 @@ CREATE TABLE `presentismo` (
 
 CREATE TABLE `privilegio` (
   `nombrePrivilegio` varchar(20) NOT NULL,
-  `numeroPerfil` int(11) NOT NULL
+  `numeroPerfil` int(11) NOT NULL,
+  `id_privilegio` int(20) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
@@ -240,7 +290,8 @@ CREATE TABLE `privilegio` (
 --
 
 CREATE TABLE `privilegioperfil` (
-  `idalgo` int(11) NOT NULL
+  `fk_perfil` int(20) NOT NULL,
+  `fk_privilegio` int(20) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
@@ -250,10 +301,12 @@ CREATE TABLE `privilegioperfil` (
 --
 
 CREATE TABLE `profesor` (
-  `apellido` varchar(50) NOT NULL,
-  `email` varchar(50) NOT NULL,
+  `id_profesor` int(20) NOT NULL,
   `legajo` varchar(20) NOT NULL,
-  `nombre` varchar(50) NOT NULL
+  `apellido` varchar(50) NOT NULL,
+  `nombre` varchar(50) NOT NULL,
+  `email` varchar(50) NOT NULL,
+  `fk_dedicacion_materia_profesor` int(20) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
@@ -263,6 +316,7 @@ CREATE TABLE `profesor` (
 --
 
 CREATE TABLE `turno` (
+  `id_turno` int(20) NOT NULL,
   `nombre` varchar(20) NOT NULL,
   `HoraDesdeTurno` time NOT NULL,
   `HoraHastaTurno` time NOT NULL
@@ -276,7 +330,11 @@ CREATE TABLE `turno` (
 
 CREATE TABLE `usuario` (
   `contraseña` varchar(50) NOT NULL,
-  `usuario` varchar(50) NOT NULL
+  `usuario` varchar(50) NOT NULL,
+  `fk_alumno` int(20) DEFAULT NULL,
+  `fk_profesor` int(20) DEFAULT NULL,
+  `fk_perfil` int(20) NOT NULL,
+  `id_usuario` int(20) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
@@ -287,7 +345,350 @@ CREATE TABLE `usuario` (
 -- Indices de la tabla `alumno`
 --
 ALTER TABLE `alumno`
-  ADD PRIMARY KEY (`legajo`);
+  ADD PRIMARY KEY (`id_alumno`),
+  ADD KEY `id_alumno` (`id_alumno`);
+
+--
+-- Indices de la tabla `anotadosestado`
+--
+ALTER TABLE `anotadosestado`
+  ADD PRIMARY KEY (`id_anotadoestado`),
+  ADD KEY `fk_detalleanotados` (`fk_detalleanotados`),
+  ADD KEY `fk_estadoanotados` (`fk_estadoanotados`);
+
+--
+-- Indices de la tabla `asueto`
+--
+ALTER TABLE `asueto`
+  ADD PRIMARY KEY (`id_asueto`),
+  ADD UNIQUE KEY `id` (`id_asueto`);
+
+--
+-- Indices de la tabla `avisoprofesor`
+--
+ALTER TABLE `avisoprofesor`
+  ADD PRIMARY KEY (`id_avisoprofesor`),
+  ADD KEY `fk_horadeconsulta` (`fk_horadeconsulta`);
+
+--
+-- Indices de la tabla `dedicacion`
+--
+ALTER TABLE `dedicacion`
+  ADD PRIMARY KEY (`id_dedicacion`);
+
+--
+-- Indices de la tabla `dedicacion_materia_profesor`
+--
+ALTER TABLE `dedicacion_materia_profesor`
+  ADD PRIMARY KEY (`id_dedicacion_materia_profesor`) USING BTREE,
+  ADD KEY `fk_dedicacion` (`fk_dedicacion`),
+  ADD KEY `fk_materia` (`fk_materia`);
+
+--
+-- Indices de la tabla `departamento`
+--
+ALTER TABLE `departamento`
+  ADD PRIMARY KEY (`id_departamento`),
+  ADD KEY `id_departamento` (`id_departamento`);
+
+--
+-- Indices de la tabla `detalleanotados`
+--
+ALTER TABLE `detalleanotados`
+  ADD PRIMARY KEY (`id_detalleanotados`),
+  ADD KEY `fk_alumno` (`fk_alumno`),
+  ADD KEY `fk_horadeconsulta` (`fk_horadeconsulta`);
+
+--
+-- Indices de la tabla `dia`
+--
+ALTER TABLE `dia`
+  ADD PRIMARY KEY (`id_dia`),
+  ADD KEY `id_dia` (`id_dia`);
+
+--
+-- Indices de la tabla `estadoanotados`
+--
+ALTER TABLE `estadoanotados`
+  ADD PRIMARY KEY (`id_estadoanotados`);
+
+--
+-- Indices de la tabla `horadeconsulta`
+--
+ALTER TABLE `horadeconsulta`
+  ADD PRIMARY KEY (`id_horadeconsulta`),
+  ADD KEY `fk_horariodeconsulta` (`fk_horariodeconsulta`),
+  ADD KEY `fk_materia` (`fk_materia`),
+  ADD KEY `fk_presentismo` (`fk_presentismo`);
+
+--
+-- Indices de la tabla `horariocursado`
+--
+ALTER TABLE `horariocursado`
+  ADD PRIMARY KEY (`id_horariocursado`),
+  ADD KEY `fk_dia` (`fk_dia`),
+  ADD KEY `fk_materia` (`fk_materia`),
+  ADD KEY `fk_profesor` (`fk_profesor`),
+  ADD KEY `fk_turno` (`fk_turno`);
+
+--
+-- Indices de la tabla `horariodeconsulta`
+--
+ALTER TABLE `horariodeconsulta`
+  ADD PRIMARY KEY (`id_horariodeconsulta`),
+  ADD KEY `fk_dia` (`fk_dia`),
+  ADD KEY `fk_materia` (`fk_materia`),
+  ADD KEY `fk_profesor` (`fk_profesor`);
+
+--
+-- Indices de la tabla `materia`
+--
+ALTER TABLE `materia`
+  ADD PRIMARY KEY (`id_materia`),
+  ADD KEY `id_materia` (`id_materia`),
+  ADD KEY `fk_dia` (`fk_dia`),
+  ADD KEY `fk_departamento` (`fk_departamento`);
+
+--
+-- Indices de la tabla `materias_alumno`
+--
+ALTER TABLE `materias_alumno`
+  ADD PRIMARY KEY (`fk_alumno`,`fk_materia`),
+  ADD KEY `fk_materia` (`fk_materia`);
+
+--
+-- Indices de la tabla `perfil`
+--
+ALTER TABLE `perfil`
+  ADD PRIMARY KEY (`id_perfil`);
+
+--
+-- Indices de la tabla `presentismo`
+--
+ALTER TABLE `presentismo`
+  ADD PRIMARY KEY (`id_presentismo`),
+  ADD KEY `fk_profesor` (`fk_profesor`);
+
+--
+-- Indices de la tabla `privilegio`
+--
+ALTER TABLE `privilegio`
+  ADD PRIMARY KEY (`id_privilegio`);
+
+--
+-- Indices de la tabla `privilegioperfil`
+--
+ALTER TABLE `privilegioperfil`
+  ADD PRIMARY KEY (`fk_perfil`,`fk_privilegio`),
+  ADD KEY `fk_privilegio` (`fk_privilegio`);
+
+--
+-- Indices de la tabla `profesor`
+--
+ALTER TABLE `profesor`
+  ADD PRIMARY KEY (`id_profesor`),
+  ADD KEY `fk_dedicacion_materia_profesor` (`fk_dedicacion_materia_profesor`);
+
+--
+-- Indices de la tabla `turno`
+--
+ALTER TABLE `turno`
+  ADD PRIMARY KEY (`id_turno`);
+
+--
+-- Indices de la tabla `usuario`
+--
+ALTER TABLE `usuario`
+  ADD PRIMARY KEY (`id_usuario`),
+  ADD KEY `fk_alumno` (`fk_alumno`),
+  ADD KEY `fk_profesor` (`fk_profesor`),
+  ADD KEY `fk_perfil` (`fk_perfil`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `anotadosestado`
+--
+ALTER TABLE `anotadosestado`
+  MODIFY `id_anotadoestado` int(20) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT de la tabla `asueto`
+--
+ALTER TABLE `asueto`
+  MODIFY `id_asueto` int(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+--
+-- AUTO_INCREMENT de la tabla `avisoprofesor`
+--
+ALTER TABLE `avisoprofesor`
+  MODIFY `id_avisoprofesor` int(20) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT de la tabla `dedicacion`
+--
+ALTER TABLE `dedicacion`
+  MODIFY `id_dedicacion` int(16) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT de la tabla `dedicacion_materia_profesor`
+--
+ALTER TABLE `dedicacion_materia_profesor`
+  MODIFY `id_dedicacion_materia_profesor` int(20) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT de la tabla `detalleanotados`
+--
+ALTER TABLE `detalleanotados`
+  MODIFY `id_detalleanotados` int(20) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT de la tabla `dia`
+--
+ALTER TABLE `dia`
+  MODIFY `id_dia` int(20) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT de la tabla `estadoanotados`
+--
+ALTER TABLE `estadoanotados`
+  MODIFY `id_estadoanotados` int(20) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT de la tabla `horariocursado`
+--
+ALTER TABLE `horariocursado`
+  MODIFY `id_horariocursado` int(20) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT de la tabla `horariodeconsulta`
+--
+ALTER TABLE `horariodeconsulta`
+  MODIFY `id_horariodeconsulta` int(20) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT de la tabla `materia`
+--
+ALTER TABLE `materia`
+  MODIFY `id_materia` int(20) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT de la tabla `perfil`
+--
+ALTER TABLE `perfil`
+  MODIFY `id_perfil` int(20) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT de la tabla `presentismo`
+--
+ALTER TABLE `presentismo`
+  MODIFY `id_presentismo` int(20) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT de la tabla `privilegio`
+--
+ALTER TABLE `privilegio`
+  MODIFY `id_privilegio` int(20) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT de la tabla `profesor`
+--
+ALTER TABLE `profesor`
+  MODIFY `id_profesor` int(20) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT de la tabla `turno`
+--
+ALTER TABLE `turno`
+  MODIFY `id_turno` int(20) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT de la tabla `usuario`
+--
+ALTER TABLE `usuario`
+  MODIFY `id_usuario` int(20) NOT NULL AUTO_INCREMENT;
+--
+-- Restricciones para tablas volcadas
+--
+
+--
+-- Filtros para la tabla `anotadosestado`
+--
+ALTER TABLE `anotadosestado`
+  ADD CONSTRAINT `anotadosestado_ibfk_1` FOREIGN KEY (`fk_detalleanotados`) REFERENCES `detalleanotados` (`id_detalleanotados`),
+  ADD CONSTRAINT `anotadosestado_ibfk_2` FOREIGN KEY (`fk_estadoanotados`) REFERENCES `estadoanotados` (`id_estadoanotados`);
+
+--
+-- Filtros para la tabla `avisoprofesor`
+--
+ALTER TABLE `avisoprofesor`
+  ADD CONSTRAINT `avisoprofesor_ibfk_1` FOREIGN KEY (`fk_horadeconsulta`) REFERENCES `horadeconsulta` (`id_horadeconsulta`);
+
+--
+-- Filtros para la tabla `dedicacion_materia_profesor`
+--
+ALTER TABLE `dedicacion_materia_profesor`
+  ADD CONSTRAINT `dedicacion_materia_profesor_ibfk_1` FOREIGN KEY (`fk_dedicacion`) REFERENCES `dedicacion` (`id_dedicacion`),
+  ADD CONSTRAINT `dedicacion_materia_profesor_ibfk_2` FOREIGN KEY (`fk_materia`) REFERENCES `materia` (`id_materia`);
+
+--
+-- Filtros para la tabla `detalleanotados`
+--
+ALTER TABLE `detalleanotados`
+  ADD CONSTRAINT `detalleanotados_ibfk_1` FOREIGN KEY (`fk_alumno`) REFERENCES `alumno` (`id_alumno`),
+  ADD CONSTRAINT `detalleanotados_ibfk_2` FOREIGN KEY (`fk_horadeconsulta`) REFERENCES `horadeconsulta` (`id_horadeconsulta`);
+
+--
+-- Filtros para la tabla `horadeconsulta`
+--
+ALTER TABLE `horadeconsulta`
+  ADD CONSTRAINT `horadeconsulta_ibfk_1` FOREIGN KEY (`fk_horariodeconsulta`) REFERENCES `horariodeconsulta` (`id_horariodeconsulta`),
+  ADD CONSTRAINT `horadeconsulta_ibfk_2` FOREIGN KEY (`fk_materia`) REFERENCES `materia` (`id_materia`),
+  ADD CONSTRAINT `horadeconsulta_ibfk_3` FOREIGN KEY (`fk_presentismo`) REFERENCES `presentismo` (`id_presentismo`);
+
+--
+-- Filtros para la tabla `horariocursado`
+--
+ALTER TABLE `horariocursado`
+  ADD CONSTRAINT `horariocursado_ibfk_1` FOREIGN KEY (`fk_dia`) REFERENCES `dia` (`id_dia`),
+  ADD CONSTRAINT `horariocursado_ibfk_2` FOREIGN KEY (`fk_materia`) REFERENCES `materia` (`id_materia`),
+  ADD CONSTRAINT `horariocursado_ibfk_3` FOREIGN KEY (`fk_profesor`) REFERENCES `profesor` (`id_profesor`),
+  ADD CONSTRAINT `horariocursado_ibfk_4` FOREIGN KEY (`fk_turno`) REFERENCES `turno` (`id_turno`);
+
+--
+-- Filtros para la tabla `horariodeconsulta`
+--
+ALTER TABLE `horariodeconsulta`
+  ADD CONSTRAINT `horariodeconsulta_ibfk_1` FOREIGN KEY (`fk_dia`) REFERENCES `dia` (`id_dia`),
+  ADD CONSTRAINT `horariodeconsulta_ibfk_2` FOREIGN KEY (`fk_materia`) REFERENCES `materia` (`id_materia`),
+  ADD CONSTRAINT `horariodeconsulta_ibfk_3` FOREIGN KEY (`fk_profesor`) REFERENCES `profesor` (`id_profesor`);
+
+--
+-- Filtros para la tabla `materia`
+--
+ALTER TABLE `materia`
+  ADD CONSTRAINT `materia_ibfk_1` FOREIGN KEY (`fk_dia`) REFERENCES `dia` (`id_dia`),
+  ADD CONSTRAINT `materia_ibfk_2` FOREIGN KEY (`fk_departamento`) REFERENCES `departamento` (`id_departamento`);
+
+--
+-- Filtros para la tabla `materias_alumno`
+--
+ALTER TABLE `materias_alumno`
+  ADD CONSTRAINT `materias_alumno_ibfk_1` FOREIGN KEY (`fk_alumno`) REFERENCES `alumno` (`id_alumno`),
+  ADD CONSTRAINT `materias_alumno_ibfk_2` FOREIGN KEY (`fk_materia`) REFERENCES `materia` (`id_materia`);
+
+--
+-- Filtros para la tabla `presentismo`
+--
+ALTER TABLE `presentismo`
+  ADD CONSTRAINT `presentismo_ibfk_1` FOREIGN KEY (`fk_profesor`) REFERENCES `profesor` (`id_profesor`);
+
+--
+-- Filtros para la tabla `privilegioperfil`
+--
+ALTER TABLE `privilegioperfil`
+  ADD CONSTRAINT `privilegioperfil_ibfk_1` FOREIGN KEY (`fk_perfil`) REFERENCES `perfil` (`id_perfil`),
+  ADD CONSTRAINT `privilegioperfil_ibfk_2` FOREIGN KEY (`fk_privilegio`) REFERENCES `privilegio` (`id_privilegio`);
+
+--
+-- Filtros para la tabla `profesor`
+--
+ALTER TABLE `profesor`
+  ADD CONSTRAINT `profesor_ibfk_1` FOREIGN KEY (`fk_dedicacion_materia_profesor`) REFERENCES `dedicacion_materia_profesor` (`id_dedicacion_materia_profesor`);
+
+--
+-- Filtros para la tabla `usuario`
+--
+ALTER TABLE `usuario`
+  ADD CONSTRAINT `usuario_ibfk_1` FOREIGN KEY (`fk_alumno`) REFERENCES `alumno` (`id_alumno`),
+  ADD CONSTRAINT `usuario_ibfk_2` FOREIGN KEY (`fk_profesor`) REFERENCES `profesor` (`id_profesor`),
+  ADD CONSTRAINT `usuario_ibfk_3` FOREIGN KEY (`fk_perfil`) REFERENCES `perfil` (`id_perfil`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
