34718f062c622d0549d4bf8ed41cfa1d6234a4c4
LucasVanden <vandenboschlucas@hotmail.com>
2019-09-19 23:00:57 -0300

    fix ruta salir

--- vista/partials/header.php
+++ vista/partials/header.php
@@ -1,4 +1,6 @@
 <header>
+<?php require_once 'C:/xampp/htdocs/ProyectoFinalUTN/vista/rutas.php';?>
   <a href="/PFProyect"> - aHora - </a>
-  <TD><a href="http://localhost:8888/ProyectoFinalUTN/vista/logout.php" style="float:right;" > - Salir -    &nbsp;&nbsp;  </a> </TD>
+  <TD><a href=<?php echo $URL."/vista/logout.php"?> style="float:right;" > - Salir -    &nbsp;&nbsp;  </a> </TD>
+
 </header>
