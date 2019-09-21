99889d345c9f567571049f475f3b121d58259638
LucasVanden <vandenboschlucas@hotmail.com>
2019-09-21 19:58:21 -0300

    fix

--- vista/rutas.php
+++ vista/rutas.php
@@ -1,47 +1,5 @@
 <?php
 
-//alumno
-// $alumnoPrincipal="'http://localhost/ProyectoFinalUTN/vista/alumno/alumnoPpal.php'";
-// $alumnoAnotaciones="'http://localhost/ProyectoFinalUTN/vista/alumno/alumnoAnotaciones.php'";
-// $alumnoConfirmarAsistencia="'http://localhost/ProyectoFinalUTN/vista/alumno/alumnoConfirmarAsistencia.php'";
-// $alumnoRegistrarAsistencia="'http://localhost/ProyectoFinalUTN/vista/alumno/alumnoRegistrarAsistencia.php'";
-// $busquedaPorMateria="'http://localhost/ProyectoFinalUTN/vista/alumno/busquedaPorMateria.php'";
-// $busquedaPorProfesor="'http://localhost/ProyectoFinalUTN/vista/alumno/busquedaPorProfesor.php'";
-
-//profesor
-$profesorAlumnosAnotados="'http://localhost/ProyectoFinalUTN/vista/profesor/profesorAlumnosAnotados.php'";
-$profesorEstablecerHorario="'http://localhost/ProyectoFinalUTN/vista/profesor/profesorEstablecerHorario.php'";
-$profesorEstablecerHorarioSE="'http://localhost/ProyectoFinalUTN/vista/profesor/profesorEstablecerHorarioSE.php'";
-$profesorNotificarAlumnos="'http://localhost/ProyectoFinalUTN/vista/profesor/profesorNotificarAlumnos.php'";
-$profesorPpal="'http://localhost/ProyectoFinalUTN/vista/profesor/profesorPpal.php'";
-$profesorRegistrarAsistencia="'http://localhost/ProyectoFinalUTN/vista/profesor/profesorRegistrarAsistencia.php'";
-$profesorRegistrarAsistenciaEgreso="'http://localhost/ProyectoFinalUTN/vista/profesor/profesorRegistrarAsistenciaEgreso.php'";
-
-//reportes
-$directorReportes="'http://localhost/ProyectoFinalUTN/vista/directorReportes.php'";
-
-
-//alumno
-//$alumnoPrincipal="http://localhost:8888/PFProyect/vista/alumno/alumnoPpal.php";
-$alumnoAnotaciones="'http://localhost:8888/ProyectoFinalUTN/vista/alumno/alumnoAnotaciones.php'";
-$alumnoConfirmarAsistencia="'http://localhost:8888/ProyectoFinalUTN/vista/alumno/alumnoConfirmarAsistencia.php'";
-$alumnoRegistrarAsistencia="'http://localhost:8888/ProyectoFinalUTN/vista/alumno/alumnoRegistrarAsistencia.php'";
-$busquedaPorMateria="'http://localhost:8888/ProyectoFinalUTN/vista/alumno/busquedaPorMateria.php'";
-$busquedaPorProfesor="'http://localhost:8888/ProyectoFinalUTN/vista/alumno/busquedaPorProfesor.php'";
-
-//profesor
-//$profesorAlumnosAnotados="'http://localhost:8888/ProyectoFinalUTN/vista/profesor/profesorAlumnosAnotados.php'";
-//$profesorEstablecerHorario="'http://localhost:8888/ProyectoFinalUTN/vista/profesor/profesorEstablecerHorario.php'";
-//$profesorEstablecerHorarioSE="'http://localhost:8888/ProyectoFinalUTN/vista/profesor/profesorEstablecerHorarioSE.php'";
-//$profesorNotificarAlumnos="'http://localhost:8888/ProyectoFinalUTN/vista/profesor/profesorNotificarAlumnos.php'";
-//$profesorPpal="'http://localhost:8888/ProyectoFinalUTN/vista/profesor/profesorPpal.php'";
-//$profesorRegistrarAsistencia="'http://localhost:8888/ProyectoFinalUTN/vista/profesor/profesorRegistrarAsistencia.php'";
-//$profesorRegistrarAsistenciaEgreso="'http://localhost:8888/ProyectoFinalUTN/vista/profesor/profesorRegistrarAsistenciaEgreso.php'";
-
-//reportes
-//$directorReportes="'http://localhost:8888/ProyectoFinalUTN/vista/directorReportes.php'";
-
-//----------------------------------------------------------------------
 $DIR='C:/xampp/htdocs/ProyectoFinalUTN';
 $URL='http://localhost:8888/ProyectoFinalUTN';
 
@@ -72,8 +30,3 @@ $DetalleAnotados='/modelo/DetalleAnotados.php';
 $EstadoAnotados='/modelo/EstadoAnotados.php';
 $AvisoProfesor='/modelo/AvisoProfesor.php';
 ?>
-
-
-<!-- <script type ='text/javascript'>
-var alumnoppal=<?php echo $alumnoPrincipal ?>;
-</script> -->
