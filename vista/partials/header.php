de38ccae896b9a7410428a580ff087fd23edc122
LucasVanden <vandenboschlucas@hotmail.com>
2019-09-21 17:38:12 -0300

    fix ir a pag principal

--- vista/partials/header.php
+++ vista/partials/header.php
@@ -1,6 +1,6 @@
 <header>
 <?php require_once 'C:/xampp/htdocs/ProyectoFinalUTN/vista/rutas.php';?>
-  <a href="/PFProyect"> - aHora - </a>
+  <a href=<?php echo $URL."/vista/alumno/alumnoPpal.php"?>> - aHora - </a>
   <TD><a href=<?php echo $URL."/vista/logout.php"?> style="float:right;" > - Salir -    &nbsp;&nbsp;  </a> </TD>
 
 </header>
