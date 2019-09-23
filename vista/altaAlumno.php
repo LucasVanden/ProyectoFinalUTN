2890d77ad12cdb9ea0d4331ecb7059a9fe12e7ff
LucasVanden <vandenboschlucas@hotmail.com>
2019-09-23 20:14:15 -0300

    validacion legajo repetido

--- vista/altaAlumno.php
+++ vista/altaAlumno.php
@@ -15,6 +15,15 @@ if (!empty($_POST['legajo']) && !empty($_POST['nombre'])&& !empty($_POST['apelli
   $telefono = $_POST['telefono'];
   $mensaje=null;
 
+
+  $stmt2 = $conexttion->prepare("SELECT id_alumno FROM alumno where legajo='$legajo'");
+  $stmt2->execute();
+  while ($row = $stmt2->fetch()) {
+    $alumno = ($row['id_alumno']);
+  }
+  if (isset($alumno)){$message="legajo existente";}else{
+
+
       $stmt = $conexttion->prepare("INSERT INTO `alumno` (`id_alumno`, `legajo`, `apellido`, `nombre`, `email`, `fechaNacimientoAlumno`,`telefonoAlumno`) 
     VALUES (NULL, '$legajo', '$apellido' , '$nombre', '$email','$fecha','$telefono');");
     if ( $stmt->execute() ){
@@ -23,8 +32,10 @@ if (!empty($_POST['legajo']) && !empty($_POST['nombre'])&& !empty($_POST['apelli
         $message="Hubo un problema al crear al alumno";
     }
 
-
+  }
  
+}else{
+    $message= 'ingrese Legajo,nombre y apellido';
 }
 ?>
 <!DOCTYPE html>
@@ -44,7 +55,7 @@ if (!empty($_POST['legajo']) && !empty($_POST['nombre'])&& !empty($_POST['apelli
     <?php endif; ?>
 
     <h1>Alta Alumno</h1>
-    <span>or <a href="altaAlumno.php">Login</a></span>
+    <span>or <a href="login.php">Login</a></span>
     <br>
     <form action="altaAlumno.php" method="POST">
     <br>
