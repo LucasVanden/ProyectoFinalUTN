dc8e10629036a4cdddf970270f6a4d04d296b9ee
LucasVanden <vandenboschlucas@hotmail.com>
2019-09-28 00:01:08 -0300

    hora dias dedicacion

--- vista/profesor/profesorEstablecerHorario.php
+++ vista/profesor/profesorEstablecerHorario.php
@@ -51,11 +51,11 @@ require_once $DIR . $profesorControlador;
                         <th>Día</th>
                         <td>
                             <select name="Dias">                       
-                                <option>Lunes</option>
-                                <option>Martes</option>
-                                <option>Miércoles</option>
-                                <option>Jueves</option>
-                                <option>Viernes</option>
+                                <option value=1>Lunes</option>
+                                <option value=2>Martes</option>
+                                <option value=3>Miércoles</option>
+                                <option value=4>Jueves</option>
+                                <option value=5>Viernes</option>
                             </select>
                         </td>
                     </tr>                   
@@ -63,8 +63,35 @@ require_once $DIR . $profesorControlador;
                         <th>Horario</th>                        
                         <td>
                             <select name="Horarios">                       
-                                <option>8:00</option>
-                                <option>8:15</option>
+                                <option value='8:00'>8:00</option>
+                                <option value='8:30'>8:30</option>
+                                <option value='9:00'>9:00</option>
+                                <option value='9:30'>9:30</option>
+                                <option value='10:00'>10:00</option>
+                                <option value='10:30'>10:30</option>
+                                <option value='11:00'>11:00</option>
+                                <option value='11:30'>11:30</option>
+                                <option value='12:00'>12:00</option>
+                                <option value='12:30'>12:30</option>
+                                <option value='13:00'>13:00</option>
+                                <option value='13:30'>13:30</option>
+                                <option value='14:00'>14:00</option>
+                                <option value='14:30'>14:30</option>
+                                <option value='15:00'>15:00</option>
+                                <option value='15:30'>15:30</option>
+                                <option value='16:00'>16:00</option>
+                                <option value='16:30'>16:30</option>
+                                <option value='17:00'>17:00</option>
+                                <option value='17:30'>17:30</option>
+                                <option value='18:00'>18:00</option>
+                                <option value='18:30'>18:30</option>
+                                <option value='19:00'>19:00</option>
+                                <option value='19:30'>19:30</option>
+                                <option value='20:00'>20:00</option>
+                                <option value='20:30'>20:30</option>
+                                <option value='21:00'>21:00</option>
+                                <option value='21:30'>21:30</option>
+                                <option value='22:00'>22:00</option>
                             </select>
                         </td>
                     </tr>                   
@@ -72,6 +99,7 @@ require_once $DIR . $profesorControlador;
             </div>
             <div>
                 <br>
+                <input type='hidden' value=<?php echo $idmateria?>></input>
                 <input type="submit" value="Establecer" name="Establecer" disabled="disabled" />
             </div>
         </form>
