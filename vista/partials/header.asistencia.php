b94bb3af5144ec25a2bc8fe57446dfd4753a6bbe
alicia <maryalis03@hotmail.com>
2019-11-07 01:00:12 -0300

    iconos que faltaban

--- vista/partials/header.asistencia.php
+++ vista/partials/header.asistencia.php
@@ -1,10 +1,10 @@
 <nav class="navbar-fixed-top">
-    <div>
+    <div><br>
 <?php require_once 'C:/xampp/htdocs/ProyectoFinalUTN/vista/rutas.php';?>
 <?php if(isset($_SESSION['nombre'])): ?>
-<TD> <strong style="float:left;">Alumno:<?php echo $_SESSION['nombre']?></strong>   &nbsp;&nbsp;  </TD>
+<TD> <strong style="float:left;"> <span class="glyphicon glyphicon-user"></span> - Alumno: <?php echo $_SESSION['nombre']?></strong>   &nbsp;&nbsp;  </TD>
 <?php endif; ?>
   <a href=<?php echo $URL.$asistenciaAlumno?>> <img src=<?php echo '"'.$URL.$logo.'"'?> width="75" height="75" /> </a>
-  <TD><a href=<?php echo $URL.$logoutasistencia?> style="float:right;" > - Salir -    &nbsp;&nbsp;  </a> </TD>
+  <TD><a href=<?php echo $URL.$logoutasistencia?> style="float:right;"><span class="glyphicon glyphicon-log-out"></span> Salir &nbsp;&nbsp;  </a> </TD>
   </div>
   </nav>
--- vista/partials/headerp.asistencia.php
+++ vista/partials/headerp.asistencia.php
@@ -1,5 +1,5 @@
 <nav class="navbar-fixed-top">
-    <div>
+    <div><br>
 <?php require_once 'C:/xampp/htdocs/ProyectoFinalUTN/vista/rutas.php';
 require_once $DIR . $profesorControlador;?>
 <?php
@@ -9,9 +9,9 @@ $_SESSION['idProfesor']=$idProfesor;
 $_SESSION['nombre']=$a->idpofesoraNombre($idProfesor);
 ?>
 <?php if(isset($_SESSION['nombre'])): ?>
-<TD> <strong style="float:left;">Profesor:<?php echo $_SESSION['nombre']?></strong>   &nbsp;&nbsp;  </TD>
+<TD> <strong style="float:left;"><span class="glyphicon glyphicon-user"></span> - Profesor: <?php echo $_SESSION['nombre']?></strong>   &nbsp;&nbsp;  </TD>
 <?php endif; ?>
   <a href=<?php echo $URL.$asistenciaProfesor?>> <img src=<?php echo '"'.$URL.$logo.'"'?> width="75" height="75" /> </a>
-  <TD><a href=<?php echo $URL.$logoutasistencia?> style="float:right;" > - Salir -    &nbsp;&nbsp;  </a> </TD>
+  <TD><a href=<?php echo $URL.$logoutasistencia?> style="float:right;"><span class="glyphicon glyphicon-log-out"></span>Salir -    &nbsp;&nbsp;  </a> </TD>
   </div>
   </nav>
