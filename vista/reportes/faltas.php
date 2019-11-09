a1ed3ac274051db81c95bb39aae57faa47f59483
LucasVanden <vandenboschlucas@hotmail.com>
2019-11-09 17:30:24 -0300

    tabla de faltas ahora es ordenable por cada atributo

--- vista/reportes/faltas.php
+++ vista/reportes/faltas.php
@@ -146,14 +146,14 @@ $_SESSION['nombre']=$a->buscarPersonalDeUsuario($idusuario);
          <?php   if(!(empty($_SESSION["faltasBuscadas"]))) : ?>
             <div class="container"> 
                 <div class="table-responsive col-md-12 col-md-offset-0">
-                    <table class="table table-bordered table-hover">
+                    <table id="myTable2" class="table table-bordered table-hover">
                         <tr class="info">
-                            <th>Legajo</th>
-                            <th>Profesor</th>
-                            <th>Tipo</th>
-                            <th>Cantidad</th>
-                            <th>Fecha</th>
-                            <th>Materia</th>
+                            <th onclick="sortTable(0)" style="cursor:pointer";>Legajo</th>
+                            <th onclick="sortTable(1)" style="cursor:pointer";>Profesor</th>
+                            <th onclick="sortTable(2)" style="cursor:pointer";>Tipo</th>
+                            <th onclick="sortTable(3)" style="cursor:pointer";>Cantidad</th>
+                            <th onclick="sortTable(4)" style="cursor:pointer";>Fecha</th>
+                            <th onclick="sortTable(5)" style="cursor:pointer";>Materia</th>
                         </tr>
                         <?php foreach ($_SESSION["faltasBuscadas"] as $falta): ?>
                         <tr>
@@ -189,6 +189,66 @@ $_SESSION['nombre']=$a->buscarPersonalDeUsuario($idusuario);
         <script src="js/jquery.js"></script>
         <script src="js/bootstrap.min.js"></script>
     </body>
+
+
+<script>
+function sortTable(n) {
+  var table, rows, switching, i, x, y, shouldSwitch, dir, switchcount = 0;
+  table = document.getElementById("myTable2");
+  switching = true;
+  // Set the sorting direction to ascending:
+  dir = "asc"; 
+  /* Make a loop that will continue until
+  no switching has been done: */
+  while (switching) {
+    // Start by saying: no switching is done:
+    switching = false;
+    rows = table.rows;
+    /* Loop through all table rows (except the
+    first, which contains table headers): */
+    for (i = 1; i < (rows.length - 1); i++) {
+      // Start by saying there should be no switching:
+      shouldSwitch = false;
+      /* Get the two elements you want to compare,
+      one from current row and one from the next: */
+      x = rows[i].getElementsByTagName("TD")[n];
+      y = rows[i + 1].getElementsByTagName("TD")[n];
+      /* Check if the two rows should switch place,
+      based on the direction, asc or desc: */
+      if (dir == "asc") {
+        if (x.innerHTML.toLowerCase() > y.innerHTML.toLowerCase()) {
+          // If so, mark as a switch and break the loop:
+          shouldSwitch = true;
+          break;
+        }
+      } else if (dir == "desc") {
+        if (x.innerHTML.toLowerCase() < y.innerHTML.toLowerCase()) {
+          // If so, mark as a switch and break the loop:
+          shouldSwitch = true;
+          break;
+        }
+      }
+    }
+    if (shouldSwitch) {
+      /* If a switch has been marked, make the switch
+      and mark that a switch has been done: */
+      rows[i].parentNode.insertBefore(rows[i + 1], rows[i]);
+      switching = true;
+      // Each time a switch is done, increase this count by 1:
+      switchcount ++; 
+    } else {
+      /* If no switching has been done AND the direction is "asc",
+      set the direction to "desc" and run the while loop again. */
+      if (switchcount == 0 && dir == "asc") {
+        dir = "desc";
+        switching = true;
+      }
+    }
+  }
+}
+</script>
+
+
     <footer class="footer">
       <?php require $DIR.$footer; ?>     
     </footer>  
