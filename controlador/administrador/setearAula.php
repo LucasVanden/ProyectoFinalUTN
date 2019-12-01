f202887e9fcc651e834fbbbe8b6144c1330f0488
LucasVanden <vandenboschlucas@hotmail.com>
2019-12-01 00:19:45 -0300

    modificar aula de consulta ahora tiene 3 campos de aula en lugar de 1 solo

--- controlador/administrador/setearAula.php
+++ controlador/administrador/setearAula.php
@@ -6,16 +6,23 @@ session_start();
 
 
 $idhorarioconsulta=$_POST['asignar'];
-$idaula=$_POST['AulaAsignada'];
+
+$cuerpo=$_POST['cuerpo'];
+$nivel=$_POST['nivel'];
+$Aula=$_POST['numeroaula'];
 
 date_default_timezone_set('America/Argentina/Mendoza');
 
 $con= new conexion();
 $conn=$con->getconexion();
 
-$stmt = $conn->prepare("UPDATE horariodeconsulta SET fk_aula = '$idaula' WHERE id_horariodeconsulta=$idhorarioconsulta"); 
+$stmt = $conn->prepare("SELECT id_aula FROM aula where cuerpoAula='$cuerpo' and nivelAula='$nivel' and numeroAula='$Aula' "); 
 $stmt->execute();
-
+while($row = $stmt->fetch()) {
+    $idaula=$row['id_aula'];
+$stmt2 = $conn->prepare("UPDATE horariodeconsulta SET fk_aula = '$idaula' WHERE id_horariodeconsulta=$idhorarioconsulta"); 
+$stmt2->execute();
+}
 
 
 $direccion= $URL . $SeleccionEditarAulaAsignada;
--- vista/administrador/SeleccionEditarAulaAsignada.php
+++ vista/administrador/SeleccionEditarAulaAsignada.php
@@ -16,6 +16,8 @@ require_once $DIR .$profesorControlador;
 require_once $DIR .$controladorAdministrador;
 $setearAula= $URL .$setearAula;
 $EditarAultaAsignada= $URL .$EditarAultaAsignada;
+$buscarNivelAula1ervacio=$URL.$buscarNivelAula1ervacio;
+$buscarNombreAula=$URL.$buscarNombreAula;
 
 require_once $DIR .$Aula;
 
@@ -44,7 +46,7 @@ header("Location: $direccion");
 
 ?>
 
-
+<!-- ANTES DE GGUARDAR -->
 
 <!DOCTYPE html>
 <html>
@@ -54,87 +56,156 @@ header("Location: $direccion");
         <link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet">
         <link href="./../assert/css/style.css" rel="stylesheet" type="text/css"/>
         <script src="./../js/funciones.js" type="text/javascript"></script>
+        <script src="jquery.js"></script>
     </head>
-    <body>
+
+    <body style="text-align: center" background = <?php echo $URL.$fondo?>>
         <?php require './../partials/header.php' ?>
         <?php if (!empty($message)): ?>
             <p> <?= $message ?></p>
         <?php endif; ?>
 
 
- <?php $a = new controladorAdministrador();
- $horarios=$a->buscarHorariosParallenarEnlosSelect($idmateria,$idprofesor);
- 
-   ?>
+        <?php $a = new controladorAdministrador();
+        $horarios=$a->buscarHorariosParallenarEnlosSelect($idmateria,$idprofesor);
+        ?>
        
         <h3>Horarios de Consulta</h3>
-       
-      
             <div>
-                <table id="tablaMateriaPpal">
+                <table id="tablaMateriaPpal" align="center">
                     <thead>                    
                         <th>Día</th>
                         <th>Horario</th>
                         <th>Semestre</th>
+                        <th>Cuerpo</th>
+                        <th>Nivel</th>
                         <th>Aula</th>
                     </thead>
-                    <body style="text-align: center" background = <?php echo $URL.$fondo?>>
+                   
                         
 
-                        <?php 
-                        foreach ($horarios as $horario): ?> 
-                            
-                      <tr>
+                    <?php foreach ($horarios as $horario): ?> 
+                    <form action=<?php echo $setearAula ?> method="POST">   
+                        <?php $salon=$a->BuscarAulaID($horario->getfk_aula()); ?>
+                        <input type="hidden" id="<?php echo "AulaidX".$horario->getid_horarioDeConsulta(); ?>"  name="idmateria" value=<?php echo $salon->getid_aula() ?>></input>
+                              
+                        <tr>
                             <td>
-                                 <?php echo $horario->getDia()->getdia(); ?>
+                                <?php echo $horario->getDia()->getdia(); ?>
                             </td>
+
                             <td>
                                 <?php echo $horario->getHora(); ?>
                             </td>
-                   <td>
-                   <?php if($horario->getsemestre()==31){echo "Mesa 1";}elseif
-                     ($horario->getsemestre()==32){echo "Mesa 2";}else{
-                         echo  $horario->getsemestre();};
-                   ?>
-                   </td>
-                   <form action=<?php echo $setearAula ?> method="POST">     
-                   <td>
-                            <select name="AulaAsignada"> 
-                            <?php
-                            $aulas=$a->BuscarAulas();
-                            foreach ($aulas as $aula): ?>
-                          
-                     
-                            <option  <?php if($horario->getfk_aula() == $aula->getid_aula()){echo("selected");}?> value=<?php echo $aula->getid_aula()?>> 
-                            
-                            <?php 
-                            echo "Cuerpo: ".$aula->getcuerpoAula()." nivel: ".$aula->getnivelAula()." aula: ".$aula->getnumeroAula();
-                            
+
+                            <td>
+                            <?php if($horario->getsemestre()==31){echo "Mesa 1";}elseif
+                                ($horario->getsemestre()==32){echo "Mesa 2";}else{
+                                    echo  $horario->getsemestre();};
                             ?>
-                            
-                            </option>
-                           
-                            <?php endforeach; ?>
-                            </select>
-                        </td>
-                        <td>
-                    <button type="submit" id="buttonAsistir" name="asignar" value=<?php echo $horario->getid_horarioDeConsulta()?> formaction=<?php echo $setearAula?>> asignar </button>
-                    </td>    
-                                </tr>
-                                </form>     
-                        <?php endforeach; 
-                            ?>
-                            
-                        
-                    </tbody>   
-                                            
+                            </td>
+  
+                            <!-- <td>
+                                <select name="AulaAsignada"> 
+                                    <?php
+                                    $aulas=$a->BuscarAulas();
+                                    foreach ($aulas as $aula): ?>
+                                    <option  <?php if($horario->getfk_aula() == $aula->getid_aula()){echo("selected");}?> value=<?php echo $aula->getid_aula()?>> 
+                                        <?php 
+                                        echo "Cuerpo: ".$aula->getcuerpoAula()." nivel: ".$aula->getnivelAula()." aula: ".$aula->getnumeroAula();
+                                        ?>
+                                    </option>
+                                
+                                    <?php endforeach; ?>
+                                </select>
+                            </td> -->
+
+                            <td>                                
+                                <select name="cuerpo"  id="<?php echo "first-choice".$horario->getid_horarioDeConsulta(); ?>" onclick="b()">
+                                            <?php 
+                                    $listacuerpoAula = $a->BuscarCuerpoAulas();
+                                    foreach ($listacuerpoAula as $cuerpoAula): ?> 
+                                    <option <?php if($salon->getcuerpoAula()==$cuerpoAula){echo ("selected");}?> value=<?php echo $cuerpoAula ?>> <?php echo $cuerpoAula  ?></option>   
+                                    <?php endforeach; 
+                                    ?>
+                                </select>
+                            </td>
+
+                            <td>                       
+                                <select  name="nivel" id="<?php echo "second-choice".$horario->getid_horarioDeConsulta(); ?>">
+                                </select> 
+                                <script>
+                                    $(<?php echo '"#first-choice'.$horario->getid_horarioDeConsulta().'"'; ?>).change(function() {
+                                    $(<?php echo '"#second-choice'.$horario->getid_horarioDeConsulta().'"';?>).load("<?php echo $buscarNivelAula1ervacio.'?choice='?>"+ $(<?php echo '"#first-choice'.$horario->getid_horarioDeConsulta().'"';?>).val()+'&aula='+ $(<?php echo '"#AulaidX'.$horario->getid_horarioDeConsulta().'"'; ?>).val());
+                                    }).change();
+                                </script>
+                            </td>
+
+                            <td>
+                                    <select name="numeroaula" id="<?php echo "profesor-choice".$horario->getid_horarioDeConsulta(); ?>">
+                                    </select>
+                                    <script>
+                                    $(<?php echo '"#second-choice'.$horario->getid_horarioDeConsulta().'"';?>).change(function() {
+                                    $(<?php echo '"#profesor-choice'.$horario->getid_horarioDeConsulta().'"'; ?>).load("<?php echo $buscarNombreAula.'?choice='?>"+ $(<?php echo '"#second-choice'.$horario->getid_horarioDeConsulta().'"';?>).val()+'&choice2='+ $(<?php echo '"#first-choice'.$horario->getid_horarioDeConsulta().'"'; ?>).val()+'&aula='+ $(<?php echo '"#AulaidX'.$horario->getid_horarioDeConsulta().'"'; ?>).val());
+                                    }).change();
+                                </script>   
+                                      <script>
+                                    $(<?php echo '"#first-choice'.$horario->getid_horarioDeConsulta().'"';?>).change(function() {
+                                    $(<?php echo '"#profesor-choice'.$horario->getid_horarioDeConsulta().'"'; ?>).load("<?php echo $buscarNombreAula.'?choice='?>"+ $(<?php echo '"#second-choice'.$horario->getid_horarioDeConsulta().'"';?>).val()+'&choice2='+ $(<?php echo '"#first-choice'.$horario->getid_horarioDeConsulta().'"'; ?>).val()+'&aula='+ $(<?php echo '"#AulaidX'.$horario->getid_horarioDeConsulta().'"'; ?>).val());
+                                    }).change();
+                                </script>                             
+                            </td>
+                            <td>
+                                <button type="submit" id="buttonAsistir" name="asignar" value=<?php echo $horario->getid_horarioDeConsulta()?> formaction=<?php echo $setearAula?>> asignar </button>
+                            </td>    
+                        </tr>
+                    </form>     
+                <?php endforeach;?>                           
                 </table>     
                 <form action=<?php echo $setearAula ?> method="POST">  
-                <div>  <input type="submit" value="Volver" name="Buscar" formaction=<?php echo $EditarAultaAsignada ?> /></div>    
+                    <div>  
+                        <input type="submit" value="Volver" name="Buscar" formaction=<?php echo $EditarAultaAsignada ?> />
+                    </div>    
                 </form>               
             </div>
-     
+
+        <!-- <button onclick="a()">piaCHU</button>        -->
     </body>
+    <script>
+    $(document).ready(function() {
+    setTimeout(function(){a()},250)
+
+});
+
+function b(){
+    setTimeout(function(){a()},250)
+}
+function a(){
+    <?php   foreach ($horarios as $horario): ?>  
+    $(<?php echo '"#second-choice'.$horario->getid_horarioDeConsulta().'"';?>).change(function() {
+    $(<?php echo '"#profesor-choice'.$horario->getid_horarioDeConsulta().'"'; ?>).load("<?php echo $buscarNombreAula.'?choice='?>"+ $(<?php echo '"#second-choice'.$horario->getid_horarioDeConsulta().'"';?>).val()+'&choice2='+ $(<?php echo '"#first-choice'.$horario->getid_horarioDeConsulta().'"'; ?>).val()+'&aula='+ $(<?php echo '"#AulaidX'.$horario->getid_horarioDeConsulta().'"'; ?>).val());
+    }).change();
+
+    <?php endforeach?>
+}
+</script>
+                                <script>
+                                    $(<?php echo '"#first-choice'.$horario->getid_horarioDeConsulta().'"'; ?>).change(function() {
+                                        $(<?php echo '"#second-choice'.$horario->getid_horarioDeConsulta().'"';?>).load("<?php echo $buscarNivelAula1ervacio.'?choice='?>"+ $(<?php echo '"#first-choice'.$horario->getid_horarioDeConsulta().'"';?>).val()+'&aula='+ $(<?php echo '"#AulaidX'.$horario->getid_horarioDeConsulta().'"'; ?>).val());
+                                        }).change();
+                                </script>
+                                <script>
+                                    $(<?php echo '"#second-choice'.$horario->getid_horarioDeConsulta().'"';?>).change(function() {
+                                    $(<?php echo '"#profesor-choice'.$horario->getid_horarioDeConsulta().'"'; ?>).load("<?php echo $buscarNombreAula.'?choice='?>"+ $(<?php echo '"#second-choice'.$horario->getid_horarioDeConsulta().'"';?>).val()+'&choice2='+ $(<?php echo '"#first-choice'.$horario->getid_horarioDeConsulta().'"'; ?>).val()+'&aula='+ $(<?php echo '"#AulaidX'.$horario->getid_horarioDeConsulta().'"'; ?>).val());
+                                    }).change();
+                                </script>
+
+                                <script>
+                                    $(<?php echo '"#first-choice'.$horario->getid_horarioDeConsulta().'"';?>).change(function() {
+                                    $(<?php echo '"#profesor-choice'.$horario->getid_horarioDeConsulta().'"'; ?>).load("<?php echo $buscarNombreAula.'?choice='?>"+ $(<?php echo '"#second-choice'.$horario->getid_horarioDeConsulta().'"';?>).val()+'&choice2='+ $(<?php echo '"#first-choice'.$horario->getid_horarioDeConsulta().'"'; ?>).val()+'&aula='+ $(<?php echo '"#AulaidX'.$horario->getid_horarioDeConsulta().'"'; ?>).val());
+                                    }).change();
+                                </script>
+
     <footer>
        <?php require $DIR.$footer; ?>         
     </footer>  
