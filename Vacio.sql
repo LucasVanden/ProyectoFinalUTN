7b800fb4ff49dd713a23a9a3a7b0c4044eb61b52
LucasVanden <vandenboschlucas@hotmail.com>
2019-12-11 17:12:34 -0300

    bd inicial

--- Vacio.sql
+++ Vacio.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 06-12-2019 a las 15:30:31
+-- Tiempo de generación: 11-12-2019 a las 21:12:08
 -- Versión del servidor: 10.1.25-MariaDB
 -- Versión de PHP: 7.1.7
 
@@ -112,8 +112,8 @@ CREATE TABLE `dedicacion` (
 --
 
 INSERT INTO `dedicacion` (`id_dedicacion`, `tipo`, `cantidadHora`) VALUES
-(1, 'doble', 2),
-(2, 'simple', 1);
+(1, 'Semi-exclusiva', 2),
+(2, 'Simple', 1);
 
 -- --------------------------------------------------------
 
@@ -331,7 +331,8 @@ INSERT INTO `perfil` (`nombrePerfil`, `id_perfil`) VALUES
 ('profesor', 2),
 ('director', 3),
 ('root', 4),
-('personal', 5);
+('personal', 5),
+('administrador', 6);
 
 -- --------------------------------------------------------
 
@@ -370,10 +371,34 @@ CREATE TABLE `presentismo` (
 --
 
 CREATE TABLE `privilegio` (
-  `nombrePrivilegio` varchar(20) NOT NULL,
-  `numeroPerfil` int(11) NOT NULL,
-  `id_privilegio` int(20) NOT NULL
+  `nombrePrivilegio` varchar(35) NOT NULL,
+  `id_privilegio` int(35) NOT NULL,
+  `numeroPermiso` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Volcado de datos para la tabla `privilegio`
+--
+
+INSERT INTO `privilegio` (`nombrePrivilegio`, `id_privilegio`, `numeroPermiso`) VALUES
+('Receso', 1, 1),
+('Feriad', 2, 2),
+('Asueto', 3, 3),
+('Mesas', 4, 4),
+('Aulas', 5, 5),
+('Depart', 6, 6),
+('Materi', 7, 7),
+('Profes', 8, 8),
+('Asigna', 9, 9),
+('Asigna', 10, 10),
+('Cambia', 11, 11),
+('Alumno', 12, 12),
+('Cargo ', 13, 13),
+('Person', 14, 14),
+('Cerrar', 15, 15),
+('Calcul', 16, 16),
+('Backup', 17, 17),
+('Admini', 18, 18);
 
 -- --------------------------------------------------------
 
@@ -385,6 +410,30 @@ CREATE TABLE `privilegioperfil` (
   `fk_perfil` int(20) NOT NULL,
   `fk_privilegio` int(20) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Volcado de datos para la tabla `privilegioperfil`
+--
+
+INSERT INTO `privilegioperfil` (`fk_perfil`, `fk_privilegio`) VALUES
+(4, 1),
+(4, 2),
+(4, 3),
+(4, 4),
+(4, 5),
+(4, 6),
+(4, 7),
+(4, 8),
+(4, 9),
+(4, 10),
+(4, 11),
+(4, 12),
+(4, 13),
+(4, 14),
+(4, 15),
+(4, 16),
+(4, 17),
+(4, 18);
 
 -- --------------------------------------------------------
 
@@ -444,7 +493,7 @@ CREATE TABLE `usuario` (
 --
 
 INSERT INTO `usuario` (`id_usuario`, `usuario`, `contraseña`, `fk_alumno`, `fk_profesor`, `fk_perfil`, `fk_persona`, `keygen`) VALUES
-(10, 'root', '$2y$10$PNwqEM24Ie1UMpkA999Z8eTrPa3.WRJ6UrU7U6sWDjCYEoP.qQs8K', NULL, 2, 4, NULL, NULL);
+(1, 'root', '$2y$10$PNwqEM24Ie1UMpkA999Z8eTrPa3.WRJ6UrU7U6sWDjCYEoP.qQs8K', NULL, NULL, 4, NULL, NULL);
 
 --
 -- Índices para tablas volcadas
@@ -652,27 +701,27 @@ ALTER TABLE `usuario`
 -- AUTO_INCREMENT de la tabla `alumno`
 --
 ALTER TABLE `alumno`
-  MODIFY `id_alumno` int(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=26;
+  MODIFY `id_alumno` int(20) NOT NULL AUTO_INCREMENT;
 --
 -- AUTO_INCREMENT de la tabla `anotadosestado`
 --
 ALTER TABLE `anotadosestado`
-  MODIFY `id_anotadoestado` int(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=259;
+  MODIFY `id_anotadoestado` int(20) NOT NULL AUTO_INCREMENT;
 --
 -- AUTO_INCREMENT de la tabla `asueto`
 --
 ALTER TABLE `asueto`
-  MODIFY `id_asueto` int(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=1224;
+  MODIFY `id_asueto` int(20) NOT NULL AUTO_INCREMENT;
 --
 -- AUTO_INCREMENT de la tabla `aula`
 --
 ALTER TABLE `aula`
-  MODIFY `id_aula` int(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=143;
+  MODIFY `id_aula` int(20) NOT NULL AUTO_INCREMENT;
 --
 -- AUTO_INCREMENT de la tabla `avisoprofesor`
 --
 ALTER TABLE `avisoprofesor`
-  MODIFY `id_avisoprofesor` int(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=44;
+  MODIFY `id_avisoprofesor` int(20) NOT NULL AUTO_INCREMENT;
 --
 -- AUTO_INCREMENT de la tabla `dedicacion`
 --
@@ -682,17 +731,17 @@ ALTER TABLE `dedicacion`
 -- AUTO_INCREMENT de la tabla `dedicacion_materia_profesor`
 --
 ALTER TABLE `dedicacion_materia_profesor`
-  MODIFY `id_dedicacion_materia_profesor` int(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=83;
+  MODIFY `id_dedicacion_materia_profesor` int(20) NOT NULL AUTO_INCREMENT;
 --
 -- AUTO_INCREMENT de la tabla `departamento`
 --
 ALTER TABLE `departamento`
-  MODIFY `id_departamento` int(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=21;
+  MODIFY `id_departamento` int(20) NOT NULL AUTO_INCREMENT;
 --
 -- AUTO_INCREMENT de la tabla `detalleanotados`
 --
 ALTER TABLE `detalleanotados`
-  MODIFY `id_detalleanotados` int(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=129;
+  MODIFY `id_detalleanotados` int(20) NOT NULL AUTO_INCREMENT;
 --
 -- AUTO_INCREMENT de la tabla `dia`
 --
@@ -707,57 +756,57 @@ ALTER TABLE `estadoanotados`
 -- AUTO_INCREMENT de la tabla `falta`
 --
 ALTER TABLE `falta`
-  MODIFY `id_falta` int(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=47;
+  MODIFY `id_falta` int(20) NOT NULL AUTO_INCREMENT;
 --
 -- AUTO_INCREMENT de la tabla `fechamesa`
 --
 ALTER TABLE `fechamesa`
-  MODIFY `id_fechaMesa` int(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=75;
+  MODIFY `id_fechaMesa` int(20) NOT NULL AUTO_INCREMENT;
 --
 -- AUTO_INCREMENT de la tabla `horadeconsulta`
 --
 ALTER TABLE `horadeconsulta`
-  MODIFY `id_horadeconsulta` int(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=125;
+  MODIFY `id_horadeconsulta` int(20) NOT NULL AUTO_INCREMENT;
 --
 -- AUTO_INCREMENT de la tabla `horariocursado`
 --
 ALTER TABLE `horariocursado`
-  MODIFY `id_horariocursado` int(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=43;
+  MODIFY `id_horariocursado` int(20) NOT NULL AUTO_INCREMENT;
 --
 -- AUTO_INCREMENT de la tabla `horariodeconsulta`
 --
 ALTER TABLE `horariodeconsulta`
-  MODIFY `id_horariodeconsulta` int(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=130;
+  MODIFY `id_horariodeconsulta` int(20) NOT NULL AUTO_INCREMENT;
 --
 -- AUTO_INCREMENT de la tabla `materia`
 --
 ALTER TABLE `materia`
-  MODIFY `id_materia` int(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=24;
+  MODIFY `id_materia` int(20) NOT NULL AUTO_INCREMENT;
 --
 -- AUTO_INCREMENT de la tabla `perfil`
 --
 ALTER TABLE `perfil`
-  MODIFY `id_perfil` int(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `id_perfil` int(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 --
 -- AUTO_INCREMENT de la tabla `persona`
 --
 ALTER TABLE `persona`
-  MODIFY `id_persona` int(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+  MODIFY `id_persona` int(20) NOT NULL AUTO_INCREMENT;
 --
 -- AUTO_INCREMENT de la tabla `presentismo`
 --
 ALTER TABLE `presentismo`
-  MODIFY `id_presentismo` int(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=37;
+  MODIFY `id_presentismo` int(20) NOT NULL AUTO_INCREMENT;
 --
 -- AUTO_INCREMENT de la tabla `privilegio`
 --
 ALTER TABLE `privilegio`
-  MODIFY `id_privilegio` int(20) NOT NULL AUTO_INCREMENT;
+  MODIFY `id_privilegio` int(35) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=20;
 --
 -- AUTO_INCREMENT de la tabla `profesor`
 --
 ALTER TABLE `profesor`
-  MODIFY `id_profesor` int(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=33;
+  MODIFY `id_profesor` int(20) NOT NULL AUTO_INCREMENT;
 --
 -- AUTO_INCREMENT de la tabla `turno`
 --
@@ -767,7 +816,7 @@ ALTER TABLE `turno`
 -- AUTO_INCREMENT de la tabla `usuario`
 --
 ALTER TABLE `usuario`
-  MODIFY `id_usuario` int(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=51;COMMIT;
+  MODIFY `id_usuario` int(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
