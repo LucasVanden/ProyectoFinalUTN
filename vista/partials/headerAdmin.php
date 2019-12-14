328918220818fb7a627c9e6efcc6549cbe21a2f9
LucasVanden <vandenboschlucas@hotmail.com>
2019-12-14 00:12:25 -0300

    logo user en admin

--- vista/partials/headerAdmin.php
+++ vista/partials/headerAdmin.php
@@ -1,9 +1,10 @@
 <nav class="fixed-top">
+<link href="//netdna.bootstrapcdn.com/font-awesome/4.0.3/css/font-awesome.css" rel="stylesheet">
     <div>
       <br>
       <?php require_once 'C:/xampp/htdocs/ProyectoFinalUTN/vista/rutas.php';?>
       <?php if(isset($_SESSION['nombre'])): ?>
-      <TD> <strong style="float:left;"><span class="glyphicon glyphicon-user"></span> - Administrador: <?php echo $_SESSION['nombre']?></strong> &nbsp;&nbsp; </TD>
+      <TD> <strong style="float:left;"><span class="fa fa-user"></span> - Administrador: <?php echo $_SESSION['nombre']?></strong> &nbsp;&nbsp; </TD>
       <?php endif; ?>
       <TD><a href=<?php echo $URL."/vista/administrador/MenuIndex.php"?> style="float:left;"> <img src=<?php echo '"'.$URL.$logo.'"'?> width="75" height="75" title="Volver"/> </a></TD>
       <TD><a href=<?php echo $URL."/vista/logout.php"?> style="float:right;"><span class="glyphicon glyphicon-log-out"></span> Salir &nbsp;&nbsp;  </a> </TD>
